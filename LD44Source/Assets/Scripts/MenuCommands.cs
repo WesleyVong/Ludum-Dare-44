c6eb11b2f8b28709eb46612d211c8a0db75a6084
WesleyVong <wv@vongs.com>
2019-04-28 20:36:00 -0700

    Organized Scripts

---
+++ LD44Source/Assets/Scripts/MenuCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCommands : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
